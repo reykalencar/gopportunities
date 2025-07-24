5edf85771a2d4a1e8b096253f17d451e071e7cfc
Reyk Alencar <reykalencar@gmail.com>
2025-07-24 09:58:15 -0300

    Adding custom logs

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/reykalencar/gopportunities/router"
+import (
+	"github.com/reykalencar/gopportunities/config"
+	"github.com/reykalencar/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initializtion error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
