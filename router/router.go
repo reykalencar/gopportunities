900f8a0a2e0e3ce03189ff4f81d7c912d11d5748
Reyk Alencar <reykalencar@gmail.com>
2025-07-24 07:01:48 -0300

    Adicionando novas rotas

--- router/router.go
+++ router/router.go
@@ -3,11 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize Routes
+	initializeRoutes(router)
+	// Run the Server
 	router.Run()
 }
